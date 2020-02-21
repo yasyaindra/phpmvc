caab51ceea146ace031588fac251afd5208f67d4
yasyaindra <yasyaindra@github.com>
2020-02-21 09:44:07 +0700

    membuat database wrapper

---
+++ app/views/about/index.php
@@ -0,0 +1,4 @@
+    <div class="container mt-4">
+    <h1>About Me</h1>
+    <p>Halo nama saya <?= $data['nama']?>, saya berumur <?= $data['umur']?> tahun. Saya <?= $data['pekerjaan'] ?></p>
+    </div>
